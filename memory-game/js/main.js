07fdc28ea9cc9e2fa9afa6239e8fd71e311f7edb
Fortuny <Fortuny@Macs-MBP.lan>
2020-01-22 00:02:02 +0100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,17 @@
 console.log("Up and running!");
 
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = []
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+let cardOne = cards[0];
+cardsInPlay.push('cardOne');
+let cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
