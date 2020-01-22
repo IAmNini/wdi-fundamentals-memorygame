a5b3a59a9afd1d6c64e5faf62ab392b340501d9b
Fortuny <fortuny.carrega@gmail.com>
2020-01-22 22:35:41 +0100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,17 +1,21 @@
-console.log("Up and running!");
-
 const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = []
+const cardsInPlay = [];
 
-let cardOne = cards[0];
-cardsInPlay.push('cardOne');
-let cardTwo = cards[2];
-cardsInPlay.push('cardTwo');
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-}
+  console.log("You found a match!");
+  } else {
+  console.log("Sorry, try again.");
+  }
+};
+
+function flipCard(cardId) {
+  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId]);
+  if (cardsInPlay.length === 2) {
+  	checkForMatch();
+  }
+};
+
+flipCard(0);
+flipCard(2);
