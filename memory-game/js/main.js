e46d6dd94ce390f817ff31e4014ae43bc1d97826
Fortuny <fortuny.carrega@gmail.com>
2020-01-23 02:06:18 +0100

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,4 +1,26 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
+}
+];
+
 const cardsInPlay = [];
 
 function checkForMatch() {
@@ -10,12 +32,15 @@ function checkForMatch() {
 };
 
 function flipCard(cardId) {
-  cardsInPlay.push(cards[cardId]);
-  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId].rank);
+  console.log("User flipped " + cards[cardId].rank);
   if (cardsInPlay.length === 2) {
   	checkForMatch();
   }
+  console.log(cards[cardId].cardImage);
+  console.log(cards[cardId].suit);
 };
 
 flipCard(0);
 flipCard(2);
+
